c5dcfd0def002b70b89d1ed77a84ed94c72c4cd5
Carson Keller <carsonjkeller@gmail.com>
2020-05-05 14:36:53 -0600

    Defined functions to access, add, and update ojects in the database

---
+++ data/glossary.js
@@ -0,0 +1,97 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+const url = process.env.DB_URL;
+
+dbName = 'glossary';
+colName = 'entries';
+
+const settings = { useUnifiedTopology : true };
+
+const getTerms = () => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, async function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to GET terms.");
+                db = client.db(dbName);
+                collection = db.collection(colName);
+                collection.find({}).toArray(function(err, docs) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(docs);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
+const addTerm = () => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, async function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to ADD term.");
+                db = client.db(dbName);
+                collection = db.collection(colName);
+                collection.insertOne(card, function(err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result.ops[0]);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
+const updateTerm = (id, term) => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, async function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to PUT term.");
+                db = client.db(dbName);
+                collection = db.collection(colName);
+                collection.replaceOne({ _id : ObjectID(id) }, 
+                term,
+                { upsert : true },
+                (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
+const archiveTerm = () => {
+
+};
+
+const deleteTerm = () => {
+
+};
+
+module.exports = {
+    getTerms,
+    addTerm,
+    updateTerm,
+    archiveTerm,
+    deleteTerm
+};
